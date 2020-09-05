fdac5b2c33de0a4776765cfbe15624015596000f
mianjvren <207921878@qq.com>
2020-09-05 09:09:53 +0800

    '提交'

--- aaa.js
+++ aaa.js
@@ -4,4 +4,3 @@ const c = 3
 const d = 4
 const e = 5
 const f = 6
-const g = 8
--- xxx.js
+++ xxx.js
@@ -4,7 +4,7 @@
 // 4.git remote add origin https://github.com/mianjvren/git.git 拷贝自己的这行代码 创建仓库
 // 5.git push -u origin master: 提交代码到 github
 
-//正常进公司以后步骤
+// 正常进公司以后步骤
 // 1.创建公钥 私钥
 
 
